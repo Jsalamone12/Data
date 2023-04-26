eea182be15235bd66bfa54272acb8ede4ad25360
Jsalamone12 <joseph.salamone12@gmail.com>
2023-04-26 18:13:54 -0400

    again

--- Controllers/ZipController.cs
+++ Controllers/ZipController.cs
@@ -1,80 +1,69 @@
-// Import necessary namespaces
 using System;
 using System.IO;
 using System.IO.Compression;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using HtmlAgilityPack;
 using Microsoft.AspNetCore.Mvc;
 
-// Define the namespace of the controller
+
 namespace Data.Controllers
 {
-    // Define the ZipController class which inherits from the Controller class
     public class ZipController : Controller
     {
-        // Define a HTTP GET action that returns the Index view
         [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
 
-        // Define a HTTP POST action that accepts a URL and clones the website
         [HttpPost]
         public async Task<IActionResult> Index(string zipUrl)
         {
-            // If the URL is empty or null, return a BadRequest response with an error message
             if (string.IsNullOrEmpty(zipUrl))
             {
                 return BadRequest("Invalid input URL");
             }
 
-            // Construct the base URL for the website
             var baseUrl = "https://" + zipUrl;
 
-            // Define the path to the folder where the website will be downloaded
             var downloadFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "GotData");
 
-            // Define the path to the zip file where the website will be stored
-            var zipFilePath = Path.Combine(downloadFolderPath, $"{zipUrl}.zip");
-
-            // If the download folder doesn't exist, create it
             if (!Directory.Exists(downloadFolderPath))
             {
                 Directory.CreateDirectory(downloadFolderPath);
             }
+            var zipFilePath = Path.Combine(downloadFolderPath, $"{zipUrl}.zip");
 
-            // Create a new ZipArchive to store the downloaded website
+            if (System.IO.File.Exists(zipFilePath))
+            {
+                var randomNumber = new Random().Next();
+                zipFilePath = Path.Combine(downloadFolderPath, $"{zipUrl}{randomNumber}.zip");
+
+            }
+
             using (var zipArchive = ZipFile.Open(zipFilePath, ZipArchiveMode.Create))
             {
-                // Call the DownloadAndZipPage method to download the website and add its contents to the ZipArchive
                 await DownloadAndZipPage(baseUrl, zipArchive);
             }
 
-            // Write a success message to the console
             Console.WriteLine($"Website cloned successfully to {zipFilePath}");
 
-            // Return the Zip view with the path to the downloaded zip file
             return View("ZipComplete", new { zipFilePath });
         }
 
-        // Define a private static method that downloads a webpage and adds its contents to a ZipArchive
         private static async Task DownloadAndZipPage(string url, ZipArchive zipArchive)
         {
             try
             {
-                // Use an HTTP client to get the HTML content of the webpage
                 var html = await new HttpClient().GetStringAsync(url);
 
-                // Parse the HTML content using HtmlAgilityPack
                 var htmlDocument = new HtmlDocument();
                 htmlDocument.LoadHtml(html);
 
-                // Get the file path for the HTML file
                 var htmlFilePath = GetHtmlFilePath(url);
 
-                // Create a new ZipArchiveEntry for the HTML file and write its contents to the ZipArchive
                 var entry = zipArchive.CreateEntry(htmlFilePath, CompressionLevel.Optimal);
                 using (var stream = entry.Open())
                 using (var writer = new StreamWriter(stream))
@@ -82,10 +71,8 @@ namespace Data.Controllers
                     writer.Write(html);
                 }
 
-                // Write a success message to the console
                 Console.WriteLine($"Downloaded {url}");
 
-                // Find all links on the webpage and download them recursively
                 var linkNodes = htmlDocument.DocumentNode.Descendants("a");
                 foreach (var linkNode in linkNodes)
                 {
@@ -96,39 +83,145 @@ namespace Data.Controllers
                         await DownloadAndZipPage(absoluteUrl, zipArchive);
                     }
                 }
+
+                var scriptNodes = htmlDocument.DocumentNode.Descendants("script");
+                foreach (var scriptNode in scriptNodes)
+                {
+                    var src = scriptNode.GetAttributeValue("src", "");
+                    if (!string.IsNullOrEmpty(src))
+                    {
+                        var absoluteUrl = new Uri(new Uri(url), src).AbsoluteUri;
+                        await DownloadAndZipFile(absoluteUrl, zipArchive);
+                    }
+                }
+
+                var linkNodesWithCss = htmlDocument.DocumentNode.Descendants("link").Where(x => x.GetAttributeValue("rel", "").Contains("stylesheet"));
+                foreach (var linkNode in linkNodesWithCss)
+                {
+                    var href = linkNode.GetAttributeValue("href", "");
+                    if (!string.IsNullOrEmpty(href))
+                    {
+                        var absoluteUrl = new Uri(new Uri(url), href).AbsoluteUri;
+                        await DownloadAndZipFile(absoluteUrl, zipArchive);
+                    }
+                }
             }
-            // Catch any exceptions that occur during the download process and write an error message to the console
             catch (Exception ex)
             {
                 Console.WriteLine($"Error downloading {url}: {ex.Message}");
             }
         }
 
-        // This method receives a URL and returns the file path where the HTML content of the page will be saved in the zip file.
+        private static async Task DownloadAndZipFile(string url, ZipArchive zipArchive)
+        {
+            try
+            {
+                var httpClient = new HttpClient();
+                var requestMessage = new HttpRequestMessage(HttpMethod.Get, url);
+
+                var response = await httpClient.SendAsync(requestMessage);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var fileContent = await response.Content.ReadAsByteArrayAsync();
+                    var fileName = Path.GetFileName(url);
+
+                    if (IsHtmlFile(url))
+                    {
+                        fileName = GetHtmlFilePath(url);
+                    }
+
+                    var entry = zipArchive.CreateEntry(fileName, CompressionLevel.Optimal);
+                    using (var stream = entry.Open())
+                    using (var writer = new BinaryWriter(stream))
+                    {
+                        writer.Write(fileContent);
+                    }
+
+                    Console.WriteLine($"Downloaded {url}");
+
+                    if (IsHtmlFile(url))
+                    {
+                        var htmlDocument = new HtmlDocument();
+                        htmlDocument.LoadHtml(Encoding.UTF8.GetString(fileContent));
+
+                        var linkNodes = htmlDocument.DocumentNode.Descendants("link");
+                        foreach (var linkNode in linkNodes)
+                        {
+                            var href = linkNode.GetAttributeValue("href", "");
+                            if (!string.IsNullOrEmpty(href) && !href.StartsWith("#") && IsCssFile(href))
+                            {
+                                var absoluteUrl = new Uri(new Uri(url), href).AbsoluteUri;
+                                await DownloadAndZipFile(absoluteUrl, zipArchive);
+                            }
+                        }
+
+                        var scriptNodes = htmlDocument.DocumentNode.Descendants("script");
+                        foreach (var scriptNode in scriptNodes)
+                        {
+                            var src = scriptNode.GetAttributeValue("src", "");
+                            if (!string.IsNullOrEmpty(src) && !src.StartsWith("#") && IsJavaScriptFile(src))
+                            {
+                                var absoluteUrl = new Uri(new Uri(url), src).AbsoluteUri;
+                                await DownloadAndZipFile(absoluteUrl, zipArchive);
+                            }
+                        }
+
+                        var iframeNodes = htmlDocument.DocumentNode.Descendants("iframe");
+                        foreach (var iframeNode in iframeNodes)
+                        {
+                            var src = iframeNode.GetAttributeValue("src", "");
+                            if (!string.IsNullOrEmpty(src) && !src.StartsWith("#"))
+                            {
+                                var absoluteUrl = new Uri(new Uri(url), src).AbsoluteUri;
+                                await DownloadAndZipFile(absoluteUrl, zipArchive);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error downloading {url}: {ex.Message}");
+            }
+        }
+
         private static string GetHtmlFilePath(string url)
         {
-            // Creates a new Uri object based on the provided URL.
             var uri = new Uri(url);
 
-            // Gets the file path by concatenating the host name and the absolute path of the URL.
             var filePath = uri.Host + uri.AbsolutePath;
 
-            // If the file path ends with a forward slash, adds "index.html" to the end of the path.
             if (filePath.EndsWith("/"))
             {
                 filePath += "index.html";
             }
-            // If the file path does not have an extension, adds "/index.html" to the end of the path.
             else if (Path.GetExtension(filePath) == "")
             {
                 filePath += "/index.html";
             }
 
-            // Removes any leading forward slashes from the file path and returns it.
             return filePath.TrimStart('/');
+        }
+
+        private static bool IsHtmlFile(string url)
+        {
+            var extension = Path.GetExtension(url);
+            return string.Equals(extension, ".html", StringComparison.OrdinalIgnoreCase)
+                || string.Equals(extension, ".htm", StringComparison.OrdinalIgnoreCase);
+        }
+
+        private static bool IsCssFile(string url)
+        {
+            var extension = Path.GetExtension(url);
+            return string.Equals(extension, ".css", StringComparison.OrdinalIgnoreCase);
+        }
+
+        private static bool IsJavaScriptFile(string url)
+        {
+            var extension = Path.GetExtension(url);
+            return string.Equals(extension, ".js", StringComparison.OrdinalIgnoreCase);
         }
     }
 }
-
-
 
